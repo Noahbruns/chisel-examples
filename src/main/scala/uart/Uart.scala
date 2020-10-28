02ceee113c75cf434a004fb4c22ac7d6c3721fd9
Noah Frederik BRUNS (ACA participant) <g01630029@ti15.tilab.tuwien.ac.at>
2020-10-28 16:02:26 +0100

    compressed code Abgabe 1

--- src/main/scala/uart/Uart.scala
+++ src/main/scala/uart/Uart.scala
@@ -202,33 +202,16 @@ class UartMain(frequency: Int, baudRate: Int) extends Module {
   val cntReg = RegInit(0.U(32.W))
   val blkReg = RegInit(0.U(1.W))
 
-  val cntRegUart = RegInit(0.U(8.W))
   val len = 1.U
 
-  when(blkReg === 1.U) {
-    val msg = "1"
-    val text = VecInit(msg.map(_.U))
-
-    tx.io.channel.bits := text(cntRegUart)
-    tx.io.channel.valid := cntRegUart =/= len
-  }
-  .otherwise {
-    val msg = "0"
-    val text = VecInit(msg.map(_.U))
-
-    tx.io.channel.bits := text(cntRegUart)
-    tx.io.channel.valid := cntRegUart =/= len
-  }
-
-  when(tx.io.channel.ready && cntRegUart =/= len) {
-    cntRegUart := cntRegUart + 1.U
-  }
+  tx.io.channel.bits := blkReg + '0'.U
+  tx.io.channel.valid := false.B
 
   cntReg := cntReg + 1.U
   when(cntReg === CNT_MAX) {
     cntReg := 0.U
     blkReg := ~blkReg
-    cntRegUart := 0.U
+    tx.io.channel.valid := true.B
   }
   io.led := blkReg
 }
